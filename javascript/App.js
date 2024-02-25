6e8abfa0d5e4c049945018fa2266c009ceb22e3c
husanjonmusayev <husanjonm279@gmail.com>
2024-02-25 18:34:34 +0500

    go

--- javascript/App.js
+++ javascript/App.js
@@ -1,5 +1,17 @@
 let btn = document.querySelectorAll(".btn");
+const hamburger = document.querySelector(".hamburger");
+const navs = document.querySelector(".navs");
+let nav = false;
 
+hamburger.addEventListener("click", function () {
+  if (!nav) {
+    navs.style.display = "block";
+    nav = true;
+  } else {
+    navs.style.display = "none";
+    nav = false;
+  }
+});
 btn.addEventListener("click", function () {
   window.location.assign("");
 });
